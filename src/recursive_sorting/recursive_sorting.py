8b600e27a19df61337cb1a68ea01981afac1d9a1
BuildTools <arthurpisakhov1@gmail.com>
2019-11-19 23:54:19 -0500

    recursive_sorting MVP

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,27 +2,36 @@
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements # creates an array of zero's based on elements val ex; [0] * 4 = [0,0,0,0]
-    # TO-DO
     for i in range(0, len(merged_arr)):
-      if  len(arrA) == 0:
+      if len(arrA) == 0:
         merged_arr[i] = arrB[0]
+        del arrB[0]
       elif len(arrB) == 0:
         merged_arr[i] = arrA[0]
+        del arrA[0]
       elif arrA[0] <= arrB[0]:
         merged_arr[i] = arrA[0]
         del arrA[0]
       elif arrA[0] >= arrB[0]:
         merged_arr[i] = arrB[0]
         del arrB[0]
-
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    if len(arr) <= 1: # Base case
+      return arr
+
     # divide in half until you have subarrays of length 1
-    # merge sorted lists together
+    mid = len(arr)//2
+    arrA = merge_sort(arr[:mid])
+    arrB = merge_sort(arr[mid:])
+    return merge(arrA, arrB)
+
     return arr
+
+# merge_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])
+
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
@@ -40,6 +49,3 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
-
-
-merge([3, 5, 6, 11], [5, 6, 7, 10])
